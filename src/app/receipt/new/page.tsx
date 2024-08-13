050eb4040071a9166f0a75a7d133d80487cda5ed
Manas Malla <manasmalla.dev@gmail.com>
2024-08-13 16:17:33 +0530

    added status
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/app/receipt/new/page.tsx
+++ src/app/receipt/new/page.tsx
@@ -30,6 +30,7 @@ export default function GenerateReceipt({ searchParams }) {
       year: "",
       color: "",
     },
+    status: false,
     notes: "",
     paymentDetails: {
       type: "Card",
