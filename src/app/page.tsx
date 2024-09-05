73a83b1d39609f1a81d7e0d57a6ca7bffcc8b924
Manas Malla <manasmalla.dev@gmail.com>
2024-09-05 10:04:21 +0530

    removed google maps
    Signed-off-by: Manas Malla <manasmalla.dev@gmail.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,34 +15,6 @@ export default function Home() {
           We aim to provide the best Service in Fastest possible turnaround
           time, while keeping the costs affordable and economical.
         </p>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-center gap-4 mt-8 items-center">
-          {[
-            { name: "Beeramguda", placeId: "" },
-            { name: "Nellore", placeId: "ChIJI5TKaXbzTDoRve9EGs1dw4Y" },
-            { name: "Uppals", placeId: "ChIJDYl2OiWZyzsRHozGYJB-qUk" },
-            { name: "KSR Layout", placeId: "" },
-            { name: "Miyapur", placeId: "ChIJccrMmluTyzsRbn4j0_5_KqE" },
-          ].map((e) => {
-            return (
-              <div
-                key={e.name}
-                className="aspect-[1.3] bg-neutral-200 rounded-2xl overflow-hidden relative"
-              >
-                <iframe
-                  className="w-full h-full border-0"
-                  loading="lazy"
-                  src={
-                    "https://www.google.com/maps/embed/v1/place?key=AIzaSyC96DnzMFaNZj5RfXeYkBH1vyZC7qSINMI&q=place_id:" +
-                    e.placeId
-                  }
-                ></iframe>
-                <p className="px-4 py-1 bg-white w-max rounded-full absolute z-10 bottom-4 left-4 cursor-pointer">
-                  {e.name}
-                </p>
-              </div>
-            );
-          })}
-        </div>
       </section>
     </div>
   );
